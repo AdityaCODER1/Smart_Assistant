fd2e032b89d1e1a76ff54029cebd26a3cd882b6d
AdityaCODER1 <86217116+AdityaCODER1@users.noreply.github.com>
2021-06-21 11:00:17 +0530

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,10 @@
+@REM This is the file to run the main.py file
+@REM Create a shortcut of this file and to enable feature in line 23 main.py, enable run as administrator in shortcut settings
+
+title [assistant name] (By, AdityaCODER1)
+prompt [assistant name]$G$S
+color 02
+cd\
+cls
+Python [main.py path]
+cls
